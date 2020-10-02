9e7f6605ba4a2134b80203212064f5fe44f5aed0
riverandeye <riverandeye@gmail.com>
2020-10-03 03:37:47 +0900

    change naming

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Route, BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
 
 import { UserProvider } from "./context/user";
-import Main from "./page/main";
+import Planner from "./page/todo";
 
 function App() {
   return (
     <Router>
       <UserProvider user={null}>
         <Switch>
-          <Route path="*" component={Main} />
+          <Route path="*" component={Planner} />
         </Switch>
       </UserProvider>
     </Router>
--- client/src/component/header/index.tsx
+++ client/src/component/header/index.tsx
@@ -5,7 +5,7 @@ import * as S from "./styles";
 const Header: React.FC = () => {
   return (
     <S.Header>
-      <S.HeaderContainer>Riverandeye's Todo</S.HeaderContainer>
+      <S.HeaderContainer>Riverandeye's Planner</S.HeaderContainer>
     </S.Header>
   );
 };
